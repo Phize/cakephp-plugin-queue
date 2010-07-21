7bee05a34be4edeefd2d437759eb4b37cb795f60
Phize <info@phize.net>
2010-07-21 20:01:59 +0000

    スキーマとSQLダンプを追加
    git-svn-id: svn://localhost/Queue/cakephp-extensions/trunk/plugins/queue@12 bc3b109d-b875-43ec-9950-69a3ddc63a01

---
+++ config/schema/schema.php
@@ -0,0 +1,53 @@
+<?php
+/* SVN FILE: $Id$ */
+/* CakephpExtensions schema generated on: 2010-07-22 04:07:19 : 1279742299*/
+class QueuePluginSchema extends CakeSchema {
+	var $name = 'QueuePlugin';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+	var $queue_jobs = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'queue_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'index'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'type' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'priority' => array('type' => 'integer', 'null' => false, 'default' => '50', 'length' => 2),
+		'recursive' => array('type' => 'boolean', 'null' => false, 'default' => '0'),
+		'interval' => array('type' => 'integer', 'null' => false, 'default' => '86400', 'length' => 10),
+		'retry_delay' => array('type' => 'integer', 'null' => false, 'default' => '60', 'length' => 10),
+		'tries' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 10),
+		'max_tries' => array('type' => 'integer', 'null' => false, 'default' => '5', 'length' => 10),
+		'parameters' => array('type' => 'text', 'null' => false, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'scheduled' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'tried' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00'),
+		'completed' => array('type' => 'datetime', 'null' => false, 'default' => '0000-00-00 00:00:00'),
+		'status' => array('type' => 'string', 'null' => false, 'default' => 'idle'),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'queue_id_name' => array('column' => array('queue_id', 'name'), 'unique' => 1), 'queue_id' => array('column' => 'queue_id', 'unique' => 0)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $queue_logs = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'job_id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'index'),
+		'status' => array('type' => 'string', 'null' => false, 'default' => NULL),
+		'message' => array('type' => 'text', 'null' => false, 'default' => NULL),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'job_id' => array('column' => 'job_id', 'unique' => 0)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+	var $queue_queues = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => NULL, 'length' => 10, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => NULL, 'key' => 'unique'),
+		'polling_delay' => array('type' => 'integer', 'null' => false, 'default' => '1', 'length' => 10),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
+		'status' => array('type' => 'string', 'null' => false, 'default' => 'stopped'),
+		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1), 'name' => array('column' => 'name', 'unique' => 1)),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+}
+?>
---
+++ config/sql/sql.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 2.11.10
+-- http://www.phpmyadmin.net
+--
+-- ホスト: localhost
+-- 生成時間: 2010 年 7 月 22 日 05:01
+-- サーバのバージョン: 5.0.91
+-- PHP のバージョン: 5.2.13
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- データベース: `development`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `queue_jobs`
+--
+
+CREATE TABLE IF NOT EXISTS `queue_jobs` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `queue_id` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL COMMENT 'ジョブ名',
+  `type` varchar(255) NOT NULL COMMENT 'ジョブタイプ',
+  `priority` tinyint(2) NOT NULL default '50' COMMENT '優先度(0 = 低, 99 = 高)',
+  `recursive` tinyint(1) NOT NULL default '0' COMMENT '繰り返し実行',
+  `interval` int(10) unsigned NOT NULL default '86400' COMMENT '繰り返し実行時の実行間隔',
+  `retry_delay` int(10) unsigned NOT NULL default '60' COMMENT 'リトライの待ち時間',
+  `tries` int(10) unsigned NOT NULL default '0' COMMENT '実行回数(リトライ用)',
+  `max_tries` int(10) unsigned NOT NULL default '5' COMMENT '最大実行回数(リトライ用)',
+  `parameters` text NOT NULL COMMENT 'パラメーター',
+  `created` datetime NOT NULL COMMENT '作成日時',
+  `scheduled` datetime NOT NULL COMMENT '次回の実行日時',
+  `tried` datetime NOT NULL default '0000-00-00 00:00:00' COMMENT '実行開始日時',
+  `completed` datetime NOT NULL default '0000-00-00 00:00:00' COMMENT '実行成功日時',
+  `status` varchar(255) NOT NULL default 'idle' COMMENT 'ステータス (idle = 実行待ち中, stopped = 停止中, running = 実行中, success = 成功, error = エラー)',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `queue_id_name` (`queue_id`,`name`),
+  KEY `queue_id` (`queue_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- テーブルのデータをダンプしています `queue_jobs`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `queue_logs`
+--
+
+CREATE TABLE IF NOT EXISTS `queue_logs` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `job_id` int(10) unsigned NOT NULL,
+  `status` varchar(255) NOT NULL COMMENT 'ステータス',
+  `message` text NOT NULL COMMENT 'メッセージ',
+  `created` datetime NOT NULL COMMENT '作成日時',
+  PRIMARY KEY  (`id`),
+  KEY `job_id` (`job_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- テーブルのデータをダンプしています `queue_logs`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `queue_queues`
+--
+
+CREATE TABLE IF NOT EXISTS `queue_queues` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL COMMENT 'キュー名',
+  `polling_delay` int(10) unsigned NOT NULL default '1',
+  `created` datetime NOT NULL COMMENT '作成日時',
+  `status` varchar(255) NOT NULL default 'stopped' COMMENT 'ステータス (stopped = 停止中, running = 稼働中)',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- テーブルのデータをダンプしています `queue_queues`
+--
+
+
+--
+-- ダンプしたテーブルの制約
+--
+
+--
+-- テーブルの制約 `queue_jobs`
+--
+ALTER TABLE `queue_jobs`
+  ADD CONSTRAINT `queue_id` FOREIGN KEY (`queue_id`) REFERENCES `queue_queues` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- テーブルの制約 `queue_logs`
+--
+ALTER TABLE `queue_logs`
+  ADD CONSTRAINT `job_id` FOREIGN KEY (`job_id`) REFERENCES `queue_jobs` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE;
